3d803fecda38c8f7f359c6aab56ce706bc9c8181
Lenul <profilprofil91@gmail.com>
2025-07-31 12:41:21 +0200

    update

---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+    props: {
+        title:{
+            
+        }
+    }
+
+};
+</script>
+
+<style scoped>
+header {
+    background-color: #42b983;
+    color: white;
+    padding: 15px;
+    text-align: center;
+}
+</style>
